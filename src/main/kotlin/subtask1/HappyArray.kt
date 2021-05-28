f387c49f1abe7beae11be3b9ab6492a7cbb03ee8
RealLuck <rlycrazy@gmail.com>
2021-05-29 01:04:50 +0300

    task1 and task2

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,28 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var sadArray = sadArray
+        var happyArray = arrayListOf<Int>()
+        var makingArrayHappy = true
+        while (makingArrayHappy){
+            for (i in sadArray.indices) {
+                if (i == 0 || i == sadArray.lastIndex) {
+                    happyArray.add(sadArray[i])
+                } else if (sadArray[i] <= (sadArray[i - 1] + sadArray[i + 1])) {
+                    happyArray.add(sadArray[i])
+                }
+            }
+                if (happyArray.size == sadArray.size) {
+                    makingArrayHappy = false
+                    return happyArray.toIntArray()
+                }
+                sadArray = happyArray.toIntArray()
+                happyArray = arrayListOf()
+
+        }
+        return sadArray
     }
 }
+
+
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,8 @@ package subtask3
 
 class StringParser {
 
-    // TODO: Complete the following function
+
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+
     }
 }
